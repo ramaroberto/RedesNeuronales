9716f0ed03d91ec9ea8ce450b54cebc4d34b6dad
Roberto Rama <bertoski@gmail.com>
2015-04-28 15:06:09 -0300

    Agregados test para Multi Perceptron

--- trunk/TP1/previoAlTP/test.m
+++ trunk/TP1/previoAlTP/test.m
@@ -1,34 +1,53 @@
 function test(group)
     
     if nargin < 1
+        %mp = MyMultiPerceptron([2, 3, 4], 0.1)
+        %clear mp;
+        return
+    end
+    
+    if strcmp(group, 'multi-xor')
+        p = MyMultiPerceptron([2, 2, 1], 0.10, 'bipolar')
+        p.layers{1}
+        p.layers{2}
+        xs = [0 0; 0 1; 1 0; 1 1];
+        zs = [-1; -1; -1; 1];
+        p.train(xs, zs, 0, 10000);
+        p.feedForward([0 0])
+        p.feedForward([0 1])
+        p.feedForward([1 0])
+        p.feedForward([1 1])
+        clear p;
+    end
+
+    if strcmp(group, 'perceptron-xor')
         p = MyPerceptron(2, 0.1, 'binary');
         xs = [0 0; 0 1; 1 0; 1 1];
         zs = [0; 1; 1; 0];
-        p.learning(xs, zs, 0, 100);
+        p.train(xs, zs, 0, 100);
         checkAnswer(xs, zs, p, 0.1)
         clear p;        
-        return
     end
-
+    
     if strcmp(group, 'perceptron-and')
         p = MyPerceptron(2, 0.25, 'scalar');
         xs = [0 0; 0 1; 1 0; 1 1];
         zs = [-1; -1; -1; 1];
-        p.learning(xs, zs, 0, 100);
+        p.train(xs, zs, 0, 100);
         checkAnswer(xs, zs, p, 0.1)
         clear p;
 
         p = MyPerceptron(2, 0.25, 'bipolar');
         xs = [0 0; 0 1; 1 0; 1 1];
         zs = [-1; -1; -1; 1];
-        p.learning(xs, zs, 0, 1000);
+        p.train(xs, zs, 0, 1000);
         checkAnswer(xs, zs, p, 0.1)
         clear p;
 
         p = MyPerceptron(2, 0.25, 'binary');
         xs = [0 0; 0 1; 1 0; 1 1];
         zs = [0; 0; 0; 1];
-        p.learning(xs, zs, 0, 1000);
+        p.train(xs, zs, 0, 1000);
         checkAnswer(xs, zs, p, 0.1)
         clear p;
     end
@@ -37,21 +56,21 @@ function test(group)
         p = MyPerceptron(2, 0.25, 'scalar');
         xs = [0 0; 0 1; 1 0; 1 1];
         zs = [-1; 1; 1; 1];
-        p.learning(xs, zs, 0, 100);
+        p.train(xs, zs, 0, 100);
         checkAnswer(xs, zs, p, 0.1)
         clear p;
         
         p = MyPerceptron(2, 0.25, 'bipolar');
         xs = [0 0; 0 1; 1 0; 1 1];
         zs = [-1; 1; 1; 1];
-        p.learning(xs, zs, 0, 1000);
+        p.train(xs, zs, 0, 1000);
         checkAnswer(xs, zs, p, 0.1)
         clear p;
         
         p = MyPerceptron(2, 0.25, 'binary');
         xs = [0 0; 0 1; 1 0; 1 1];
         zs = [0; 1; 1; 1];
-        p.learning(xs, zs, 0, 1000);
+        p.train(xs, zs, 0, 1000);
         checkAnswer(xs, zs, p, 0.1)
         clear p;
     end
