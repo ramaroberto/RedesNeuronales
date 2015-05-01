7208928ffe7b21f6a8561d3fc91be0c76ad1d59f
fnlandini <federico91_fnl@yahoo.com.ar>
2015-05-01 14:48:36 -0300

    modo en guardar y cargar

--- trunk/TP1/código/guardar.m
+++ trunk/TP1/código/guardar.m
@@ -1,9 +1,18 @@
-function [] = guardar(fileName, inputSize, layers, outputSize, weights)
+% Guarda con la siguiente estructura
+% cantidad_de_capas cantidad_de_atributos cantidad_de_salida cantidad_de_matrices_weight
+% string_modo
+% cantidad de neuronas (sin bias) en cada capa oculta
+% por cada peso se escribe la matriz respetando las filas y separando los elementos con espacios. Una abajo de la otra
+function [] = guardar(fileName, inputSize, layers, outputSize, weights, modo)
 	fileID = fopen(fileName,'w');
 	fprintf(fileID, '%d ', length(layers));
 	fprintf(fileID, '%d ', inputSize);
 	fprintf(fileID, '%d ', outputSize);
 	fprintf(fileID, '%d ', length(weights));
+	fprintf(fileID, '\n');
+	fprintf(fileID, '%s', modo);
+	fprintf(fileID, '\n');
+
 	% Guardar todos los valores de las capas ocultas
 	for i = 1:length(layers)
 		fprintf(fileID, '%d ',layers(i));
