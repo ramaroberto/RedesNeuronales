e0d575d679c581989a162c63949d0d3893d3c3b2
fnlandini <federico91_fnl@yahoo.com.ar>
2015-06-14 12:06:37 -0300

    comentarios sobre como usar usarHebbiano en el codigo

--- trunk/TP2/src/hebbiano/usarHebbiano.m
+++ trunk/TP2/src/hebbiano/usarHebbiano.m
@@ -1,3 +1,14 @@
+% criterioParada es 'p' o 'o' segun se quiera corte por delta pesos u ortogonalidad
+% regla es 's' o 'M' segun se quiera usar Sanger u Oja (M)
+% learningRate es el valor de learningRate a usar
+% alpha es el valor en la formula learningRate = epoca^(-alpha) para actualizar el learning rate. Si alpha es 0 entonces esa formula no se usa
+% calcularPesos es un bool que cuando es true indica que se calcule la matriz de pesos mientras que cuando es false indica que se lea una matriz ya creada
+% cantEpocas es la cantidad de epocas maxima que se admite (para tener otro criterio de corte)
+% trainFilename es el nombre (path completo) del archivo donde estan las instancias de entrenamiento
+% testFilename es el nombre (path completo) del archivo donde estan las instancias de validacion
+% weightsFilename es el nombre (path completo) del archivo donde se guardara (o desde donde se leera si calcularPesos=false) la matriz de pesos
+% datosFilename es el nombre del archivo donde se escribira la informacion sobre la corrida
+% figureFilename es el nombre del .fig que se creara con el grafico de las instancias
 function [weights, razon, epocaOError] = usarHebbiano(criterioParada, regla, learningRate, alpha, calcularPesos, cantEpocas, trainFilename, testFilename, weightsFilename, datosFilename, figureFilename)
 	
 	trainInput = csvread(trainFilename, 0, 1);
@@ -27,15 +38,15 @@ function [weights, razon, epocaOError] = usarHebbiano(criterioParada, regla, lea
 	resTest = csvread(testFilename, 0, 0, [0 0 numberTest-1 0]);
 	group = [resTrain;resTest];
 
-	colores = [	[1 0 1];
-				[0 1 1];
-				[1 0 0];
-				[0 1 0];
-				[0 0 1];
-				[1 1 0];
-				[0.5 0.5 0];
-				[0.5 0 0.5];
-				[0 0.5 0.5]];
+	colores = [	[1 0 1];		% magenta
+				[0 1 1];		% cian
+				[1 0 0];		% rojo
+				[0 1 0];		% verde
+				[0 0 1];		% azul
+				[1 1 0];		% amarillo
+				[0.5 0.5 0];	% marrón
+				[0.5 0 0.5];	% violeta
+				[0 0.5 0.5]];	% azul oscuro
 
 	h=figure('visible','on');
 	for idx = 1:9
