a1c8666b54897b24b5041c6fe0ef66d798902e4f
ramaroberto <bertoski@gmail.com>
2015-06-14 23:11:56 -0300

    Modificaciones al script de utilizar Kohonen

--- trunk/TP2/src/kohonen/usarKohonen.m
+++ trunk/TP2/src/kohonen/usarKohonen.m
@@ -1,21 +1,25 @@
-function [weights, epoca] = main(learningRate, sigma, autoajuste, M1, M2, calcularPesos, cantEpocas, trainFilename, testFilename, weightsFilename, figureTrainFilename, figureTestFilename, datosFilename)
+function [weights] = usarKohonen(learningRate, sigma, autoajuste, M1, M2, calcularPesos, cantEpocas, trainFilename, testFilename, weightsFilename, figureTrainFilename, figureTestFilename, datosFilename)
 	trainInput = csvread(trainFilename, 0, 1);
 	train = csvread(trainFilename, 0, 0);
-	testInput = csvread(testFilename, 0, 1);
 	test = csvread(testFilename, 0, 0);
 
 	if calcularPesos
-		[weights] = som( trainInput, M1, M2, cantEpocas, learningRate, sigma, autoajuste );
+		[weights] = som( trainInput, M1, M2, cantEpocas, learningRate, sigma, autoajuste, false );
 		csvwrite(weightsFilename,weights);
 	else
 		weights = csvread(weightsFilename);
-	end
-	fileID = fopen(datosFilename,'a');
-	fprintf(fileID,'learningRate: %f \t sigma: %f \t autoajuste: %d \t M1,M2: %d \n', learningRate, sigma, autoajuste, M1, M2);
-	fclose(fileID);
+    end
 
-	trainActivados = aplicarPesos(weights,train,M1,M2);
-	testActivados = aplicarPesos(weights,test,M1,M2);
+	[trainActivados, counts] = aplicarPesos(weights,train,M1,M2);
+	[testActivados, ~] = aplicarPesos(weights,test,M1,M2);
+    
+    [rel_dist, avg_dist, ~] = distanceToDominants(trainActivados, testActivados, false);
+    [rel_dist_man, avg_dist_man, ~] = distanceToDominants(trainActivados, testActivados, true);
+    [sharing_factor, ~] = getSharingFactor(counts);
+    
+    fileID = fopen(datosFilename,'a');
+	fprintf(fileID,'learningRate: %f \t sigma: %f \t autoajuste: %d \t M1,M2: %d \t rel_dist: %.3f \t avg_dist %.3f \t rel_dist_m: %.3f \t avg_dist_m: %.3f \t sharing: %.3f \n', learningRate, sigma, autoajuste, M1, M2, rel_dist, avg_dist, rel_dist_man, avg_dist_man, sharing_factor);
+	fclose(fileID);
 	
 	h = imagesc(trainActivados);
 	axis square;
@@ -24,9 +28,9 @@ function [weights, epoca] = main(learningRate, sigma, autoajuste, M1, M2, calcul
 	colorbar;
 	saveas(h,figureTrainFilename,'fig');
 
-	h = imagesc(testActivados);
+	g = imagesc(testActivados);
 	axis square;
 	caxis([0 9]);
 	colormap(jet(10));
 	colorbar;
-	saveas(h,figureTestFilename,'fig');
+	saveas(g,figureTestFilename,'fig');
